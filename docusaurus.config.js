bd67dec2e9e2e9bdd3381faf3e911782e2f329f1
jo32 <j.jolam@gmail.com>
2023-09-20 20:22:28 +0800

    feat: update editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,14 +67,14 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/MegaPortal/closevector-doc/tree/main',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/MegaPortal/closevector-doc/tree/main',
                 },
                 theme: {
                     customCss: [
