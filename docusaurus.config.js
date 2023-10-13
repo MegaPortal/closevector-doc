a1c44592cb417db023b38ad3fa885a66b70100e9
jo32 <j.jolam@gmail.com>
2023-10-13 17:35:55 +0800

    feat: update social img

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
             // Replace with your project's social card
-            image: 'img/docusaurus-social-card.jpg',
+            image: 'img/social-card.png',
             navbar: {
                 title: 'CloseVector',
                 logo: {
